c55186e7df8cfc52010d9d65de8ad1d859b02a23
RichardLimaDxD <richardpires663802@gmail.com>
2024-09-08 22:49:03 -0300

    feat: create enums and entities

---
+++ src/Backend/MyRecipeBook.Domain/Entities/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using MyRecipeBook.Domain.Enums;
+
+namespace MyRecipeBook.Domain.Entities
+{
+    public class Recipe : EntityBase
+    {
+        public string Title { get; set; } = string.Empty;
+
+        public CookingTime? CookingTime { get; set; }
+
+        public Difficulty? Difficulty { get; set; }
+
+        public IList<Ingredient> Ingredients { get; set; } = [];
+
+        public IList<Instruction> Instruction { get; set; } = [];
+
+        public IList<DishType> DishTypes { get; set; } = [];
+
+        public long UserId { get; set; }
+    }
+}
